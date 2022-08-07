bf499d11a981fbb6740020077cbc7bc7ef0cdd67
Jedaias Farias <jedafarias@gmail.com>
2022-08-07 16:11:45 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/IgrejaBatista/NovaVida/BazarOfertaCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.IgrejaBatista.NovaVida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarOfertaCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarOfertaCatalogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IgrejaBatista/NovaVida/BazarOfertaCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IgrejaBatista.NovaVida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarOfertaCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
