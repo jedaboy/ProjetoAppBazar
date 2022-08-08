390bc0ac0273c39d83d5f9255a644f9107300a94
Jedaias Farias <jedafarias@gmail.com>
2022-08-07 23:26:21 -0300

    Criação das primeiras classes, de resource e domain

---
+++ src/main/java/com/IgrejaBatista/NovaVida/resources/CategoriaResource.java
@@ -0,0 +1,10 @@
+package com.IgrejaBatista.NovaVida.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+}
